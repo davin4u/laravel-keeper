dc7ea69688285d0cb53c6dc3b32c9a062537bf44
Denis Gorgul <davin4u@gmail.com>
2017-10-11 09:55:23 +0300

    change app name, change menu icons

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -9,7 +9,7 @@
                 </ul>
             </li>
             <li>
-                <a><i class="fa fa-home"></i> Projects <span class="fa fa-chevron-down"></span></a>
+                <a><i class="fa fa-folder"></i> Projects <span class="fa fa-chevron-down"></span></a>
                 <ul class="nav child_menu">
                     @foreach (menu('projects') as $link => $item)
                     <li><a href="{{ $link }}">{{ $item }}</a></li>
@@ -18,7 +18,7 @@
                 </ul>
             </li>
             <li>
-                <a><i class="fa fa-home"></i> Passwords <span class="fa fa-chevron-down"></span></a>
+                <a><i class="fa fa-lock"></i> Passwords <span class="fa fa-chevron-down"></span></a>
                 <ul class="nav child_menu">
                   @foreach (menu('passwords') as $link => $item)
                   <li><a href="{{ $link }}">{{ $item }}</a></li>
@@ -26,7 +26,7 @@
                 </ul>
             </li>
             <li>
-                <a><i class="fa fa-home"></i> Settings <span class="fa fa-chevron-down"></span></a>
+                <a><i class="fa fa-gears"></i> Settings <span class="fa fa-chevron-down"></span></a>
                 <ul class="nav child_menu">
                   <li><a href="/settings/profile">Profile</a></li>
                   <li><a href="/settings/password-types">Password Types</a></li>
